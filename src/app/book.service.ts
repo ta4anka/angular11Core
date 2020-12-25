231cdef2abe22af7f0bfdb490ef26ff624e0302b
ta4anka <vladymirsheyko@gmail.com>
2020-12-25 22:15:39 +0200

    added task_7

---
+++ src/app/book.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import {Book} from './model/book';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BookService {
+
+  books: Book[];
+
+  constructor() {
+    this.books = [
+      {
+        author: 'Douglas Crockford', year: 2008, title: 'JavaScript: The Good Parts', pages: 172,
+        description: 'This authoritative book scrapes away these bad features to reveal a subset of JavaScript that\'s more reliable, readable, and maintainable'
+      },
+      {
+        author: 'Nathan Rozentals', year: 2015, title: 'Mastering TypeScript', pages: 364,
+        description: 'Build enterprise-ready, industrial strength web applications using TypeScript and leading JavaScript frameworks'
+      },
+      {author: 'Mishko Hevery', year: 2020, title: 'Angular 11'}
+    ];
+  }
+
+  getBooks(): Book[] {
+    return this.books;
+  }
+}
--- src/app/books/books.component.ts
+++ src/app/books/books.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {Book} from '../model/book';
+import {BookService} from '../book.service';
 
 @Component({
   selector: 'app-books',
@@ -7,23 +8,13 @@ import {Book} from '../model/book';
   styleUrls: ['./books.component.css']
 })
 export class BooksComponent implements OnInit {
-  books: Book[];
+  books: Book[] | undefined;
 
-  constructor() {
-    this.books = [
-      {
-        author: 'Douglas Crockford', year: 2008, title: 'JavaScript: The Good Parts', pages: 172,
-        description: 'This authoritative book scrapes away these bad features to reveal a subset of JavaScript that\'s more reliable, readable, and maintainable'
-      },
-      {
-        author: 'Nathan Rozentals', year: 2015, title: 'Mastering TypeScript', pages: 364,
-        description: 'Build enterprise-ready, industrial strength web applications using TypeScript and leading JavaScript frameworks'
-      },
-      {author: 'Mishko Hevery', year: 2020, title: 'Angular 11'}
-    ];
+  constructor(private bookService: BookService) {
   }
 
   ngOnInit(): void {
+    this.books = this.bookService.getBooks();
   }
 
   isJavaScript(book: Book): boolean {
