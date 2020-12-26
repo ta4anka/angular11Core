129aedfd20a912f58b04f73375ffe9578c2e109c
ta4anka <vladymirsheyko@gmail.com>
2020-12-26 23:40:09 +0200

    added task_12

--- src/app/book/book.component.ts
+++ src/app/book/book.component.ts
@@ -1,43 +1,17 @@
-import {AfterContentChecked, Component, OnChanges, OnDestroy, OnInit, SimpleChanges} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {Book} from '../model/book';
-import {StringOrNumber} from '../model/custom-types';
 
 @Component({
   selector: 'app-book',
   templateUrl: './book.component.html',
   styleUrls: ['./book.component.css']
 })
-export class BookComponent implements OnInit, OnChanges, OnDestroy, AfterContentChecked {
-  readonly book: Book;
-  characterCount: number | undefined;
+export class BookComponent implements OnInit {
+  @Input() book: Book | undefined;
 
   constructor() {
-    this.book = {author: 'Stephen King', title: 'Rita Hayworth and the Shawshank Redemption', year: 1977};
   }
 
   ngOnInit(): void {
   }
-
-  ngAfterContentChecked(): void {
-    console.log('AfterContentChecked');
-  }
-
-  ngOnChanges(changes: SimpleChanges): void {
-    console.log('OnChanges');
-  }
-
-  ngOnDestroy(): void {
-    console.log('OnDestroy');
-  }
-
-  changeCharacterCount(value: StringOrNumber, event?: MouseEvent): void {
-    if (value) {
-      this.characterCount = String(value).length;
-    } else {
-      this.characterCount = 0;
-    }
-    console.log('Current X ' + event?.clientX);
-    console.log('Current Y ' + event?.clientY);
-  }
-
 }
