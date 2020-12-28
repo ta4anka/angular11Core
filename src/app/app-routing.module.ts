9f88067df8a45e8a715f695c211d7e58fa18d67b
ta4anka <vladymirsheyko@gmail.com>
2020-12-28 21:00:13 +0200

    added task_19

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {BooksComponent} from './books/books.component';
+import {NewsComponent} from './news/news/news.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'books', pathMatch: 'full'},
+  {path: 'books', component: BooksComponent},
+  {path: 'news', component: NewsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
